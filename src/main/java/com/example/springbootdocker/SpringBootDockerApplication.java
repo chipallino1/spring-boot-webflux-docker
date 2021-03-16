7b0f025cb5e21068483e7ec124b158a39f3eaa42
chipallino1 <skorupich00@mail.ru>
2021-03-16 17:22:12 +0300

    Added rest controller and webflux dependency.

--- src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -2,12 +2,21 @@ package com.example.springbootdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDockerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+
+    @GetMapping("/docker")
+    public Mono<String> helloDocker() {
+        return Mono.just("Hello, docker!");
+    }
 
 }
